584fdba48db466cfe8fe757e01bc4527bedfe0c7
Mike Perugini <11peruginiM@gmail.com>
2015-12-09 01:27:22 -0500

    Added index.php

---
+++ perugini.co/public_html/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lan="en">
+<head>
+	<title>Welcome to Perugini.co!</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/darkly/bootstrap.css">
+</head>
+<body>
+    
+    <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.html" class="navbar-brand">perugini.co</a>
+            </div>
+            <div class="navbar-collapse collapse" id="navbar-main">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="">Resume</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="">Login/Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    </br>
+    </br>
+    </br>
+    
+	<div class="container">
+		<h1>Success!  The perugini.co virtual host is working! Adding navbar.</h1>
+	</div>
+</body>
+</html>
