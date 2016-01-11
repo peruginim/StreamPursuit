2f6c603b42f7383036dde0e665236b70aff0adeb
Michael Perugini <perugini@Michaels-MacBook-Pro.local>
2016-01-11 13:14:30 -0500

    Wrote datagrab script that continuously pulls viewer data and pushes it to the data stream.

---
+++ perugini.co/scripts/datagrab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [ 1 ]; do
+	# Grab Viewer count and Timestamp; Save them in respective varaibles
+	VWRS="$(curl -s 'https://api.twitch.tv/kraken/streams/summary' | jsawk 'return this.viewers')"
+	TIME="$(date +"%H:%M")"
+
+	# Post this data to our sparkfun data stream
+	curl -s -o /dev/null -X POST 'http://data.sparkfun.com/input/NJGqaM0GGbtRrOrd3DOd' \
+	-H 'Phant-Private-Key: 5d9g6jK99msWNdNJYBdJ' \
+	-d "viewers=$VWRS&time=$TIME"
+done
