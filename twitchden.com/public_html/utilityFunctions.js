c438fe0af728118ca4dc3e8d23188dbb562a4b5d
Michael Perugini <perugini@Michaels-MacBook-Pro.local>
2016-01-19 10:30:22 -0500

    Added sanitizeGame function to replace spaces with %20

--- twitchden.com/public_html/utilityFunctions.js
+++ twitchden.com/public_html/utilityFunctions.js
@@ -1,15 +1,32 @@
-function getRandom() {
+function getRandom()
+{
   return Math.random();
 }
 
-function getRandomArbitrary(min, max) {
+function getRandomArbitrary(min, max)
+{
   return Math.random() * (max - min) + min;
 }
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max)
+{
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min, max)
+{
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sanitizeGame(game)
+{
+    var newString = "";
+    for(var i = 0; i < game.length; i++)
+    {
+        if(game[i].localeCompare(" ") == 0)
+            newString += "%20";
+        else
+            newString += game[i];
+    }
+    return newString;
 }
