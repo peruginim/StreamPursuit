2863cb6665884072d422462df1d31b3bd63f9317
Michael Perugini <perugini@Michaels-MacBook-Pro.local>
2016-01-21 13:04:17 -0500

    Added two follow functions. One uses purely the Twitch SDK to run the PUT request, but it's running into issues. Most likely the issues stem from the version of twitch.min.js that I'm using. Posted on developer forums about it. Also added a follow function that uses both the api and regular xmlhttp requests. This one returns a 401 error so there's something wrong with authentication. Again, posted about this on Twitch Developer Forums. Waiting for help on solution.

--- twitchden.com/public_html/utilityFunctions.js
+++ twitchden.com/public_html/utilityFunctions.js
@@ -30,3 +30,92 @@ function sanitizeGame(game)
     }
     return newString;
 }
+
+function followAPI(channel)
+{
+    var success = function()
+    {
+        // Change color of button to green and change 'Follow' to 'Followed'
+        document.getElementById("follow-button").innerHTML = "Followed";
+        document.getElementById("follow-button").className = "btn btn-success btn-lg btn-block";
+        document.getElementById("follow-button").removeAttribute("onclick");
+    }
+    var failure = function(step)
+    {
+        // Popup saying 'Something broke, make sure you're logged into twitch.'
+        //var token = Twitch.getToken();
+        window.alert("Something went wrong at step: " + step);
+    }
+
+    Twitch.getStatus(function(error, status)
+    {
+        if(error) return failure("1");
+        if(!status.authenticated) return failure("-1");
+
+        Twitch.api({method: 'user'}, function(error, user)
+        {
+            if(error) return failure("2");
+            var xmlHttp = new XMLHttpRequest();
+            var url = "https://api.twitch.tv/kraken/users/" + user.name + "/follows/channels/"
+            + channel + "?oauth_token=[" + Twitch.getToken() + "]";
+            xmlHttp.open('PUT', url, true);
+            xmlHttp.onreadystatechange = function()
+            {
+                //if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+                if(xmlHttp.readyState === 4)
+                {
+                    var check = xmlHttp.status;
+                    alert(check);
+                    if(check.toString()==="200")
+                    {
+                        success();
+                    }
+                    //success();
+                }
+                else
+                {
+                    failure("3");
+                }
+            };
+            xmlHttp.setRequestHeader("Content-Type", "application/json");
+            xmlHttp.setRequestHeader("Accept", "application/vnd.twitchtv.v3+json");
+            //xmlHttp.setRequestHeader("Authorization", "OAuth " + Twitch.getToken());
+            xmlHttp.setRequestHeader("Content-Length", "0");
+            xmlHttp.send();
+        });
+    });
+}
+
+function follow(channel)
+{
+    var success = function()
+    {
+        // Change color of button to green and change 'Follow' to 'Followed'
+        document.getElementById("follow-button").innerHTML = "Followed";
+        document.getElementById("follow-button").className = "btn btn-success btn-lg btn-block";
+        document.getElementById("follow-button").removeAttribute("onclick");
+    }
+    var failure = function(step)
+    {
+        // Popup saying 'Something broke, make sure you're logged into twitch.'
+        var token = Twitch.getToken();
+        window.alert("Something went wrong at step: " + token);
+    }
+
+    Twitch.getStatus(function(error, status)
+    {
+        if(error) return failure("1");
+        if(!status.authenticated) return failure("-1");
+
+        Twitch.api({method: 'user'}, function(error, user)
+        {
+            if(error) return failure("2");
+
+            Twitch.api({verb: 'PUT', method: 'users/' + user.name + '/follows/channels/' + channel}, function(error, response)
+            {
+                if(error) return failure(user.name);
+                success();
+            });
+        });
+    });
+}
