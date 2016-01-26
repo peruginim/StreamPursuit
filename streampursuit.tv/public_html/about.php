87bb43769a591aab93459f3cf43d9b6d3744d662
peruginim <11peruginiM@gmail.com>
2016-01-26 16:27:21 -0500

    Changed favicon

--- streampursuit.tv/public_html/about.php
+++ streampursuit.tv/public_html/about.php
@@ -17,22 +17,22 @@ session_start();
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <link rel="icon" href="favicon.ico" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="57x57" href="images/twitchicon.ico/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="images/twitchicon.ico/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="images/twitchicon.ico/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="images/twitchicon.ico/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="images/twitchicon.ico/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="images/twitchicon.ico/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="images/twitchicon.ico/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="images/twitchicon.ico/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="images/twitchicon.ico/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="images/twitchicon.ico/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="images/twitchicon.ico/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="images/twitchicon.ico/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="images/twitchicon.ico/favicon-16x16.png">
-        <link rel="manifest" href="images/twitchicon.ico/manifest.json">
+        <link rel="apple-touch-icon" sizes="57x57" href="images/streampursuit.ico/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="images/streampursuit.ico/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/streampursuit.ico/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="images/streampursuit.ico/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/streampursuit.ico/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="images/streampursuit.ico/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="images/streampursuit.ico/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="images/streampursuit.ico/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/streampursuit.ico/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="images/streampursuit.ico/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/streampursuit.ico/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="images/streampursuit.ico/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/streampursuit.ico/favicon-16x16.png">
+        <link rel="manifest" href="images/streampursuit.ico/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="images/twitchicon.ico/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="images/streampursuit.ico/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
         <script src="httpFunctions.js"></script>
         <script>
